5d50e47d15d4c05556ad11810fe432414373879a
atomjaylee <908105576@qq.com>
2021-08-24 16:10:28 +0800

    feat: 打包最新组件并发布最新1.0.7版本

--- components/selector/selector.js
+++ components/selector/selector.js
@@ -1,6 +1,20 @@
-const defaultProps = {};
+const defaultProps = {
+  title: '',
+  confirmText: '确定',
+  schema: [],
+  hiddenCloseIcon: false,
+  maskClosable: true,
+  activeLabel: 'label',
+  activeKey: 'key',
+  activeChildren: 'children',
+  activeDisabled: 'disabled',
+  onBeforeClose: () => true
+};
 Component({
   props: defaultProps,
+  data: {
+    checkedList: []
+  },
   methods: {
     popup(ref) {
       this.$popup = ref;
@@ -8,6 +22,47 @@ Component({
 
     show() {
       this.$popup.show();
+    },
+
+    close() {
+      this.$popup.close();
+    },
+
+    async onConfirmHandler() {
+      const isPass = await this.props.onBeforeClose(this.data.checkedList[0]);
+
+      if (isPass) {
+        this.close();
+        this.props.onConfirm && this.props.onConfirm(this.data.checkedList[0]);
+      }
+    },
+
+    // 点击关闭时模拟点击遮罩层关闭，从而触发onCancel方法
+    mockMaskClose() {
+      this.$popup.onMaskTapHandler({
+        target: {
+          targetDataset: {
+            nodeName: 'mask'
+          }
+        }
+      });
+    },
+
+    // 代理onAfterClose事件
+    proxyAfterClose() {
+      this.props.onAfterClose && this.props.onAfterClose();
+    },
+
+    // 代理onCancel事件
+    proxyCancel() {
+      this.props.onCancel && this.props.onCancel();
+    },
+
+    // checkGroup修改
+    onGroupCheckChangeHandler(checkedList) {
+      this.setData({
+        checkedList
+      });
     }
 
   }
---
+++ components/selector/selectorItem/selectorItem.js
@@ -0,0 +1,47 @@
+const defaultProps = {
+  source: {},
+  activeLabel: 'label',
+  activeKey: 'key',
+  activeChildren: 'children',
+  activeDisabled: 'disabled'
+};
+Component({
+  props: defaultProps,
+  data: {
+    // 折叠控制
+    foldControl: {}
+  },
+  methods: {
+    checkInstance(ref) {
+      this.$checkInstance = ref;
+    },
+
+    // 展开和折叠子选项
+    onFoldItemHandler({
+      target: {
+        dataset
+      }
+    }) {
+      const key = dataset.key;
+      const currentState = this.data.foldControl[key];
+      this.setData({
+        foldControl: { ...this.data.foldControl,
+          [key]: !currentState
+        }
+      });
+    },
+
+    // 通过点击body选中check
+    onCheckedByBodyHandler({
+      target: {
+        targetDataset
+      }
+    }) {
+      if (targetDataset.nodeName !== 'check') {
+        this.$checkInstance.onCheckTapHandler();
+      }
+    }
+
+  }
+});
+export {};
