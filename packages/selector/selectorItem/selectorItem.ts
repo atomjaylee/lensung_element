9f3f7d9fa4ca08824a17e5d46d2efafbda9f15dd
atomjaylee <908105576@qq.com>
2021-08-24 16:08:52 +0800

    feat: 增加selector组件折叠逻辑

--- packages/selector/selectorItem/selectorItem.ts
+++ packages/selector/selectorItem/selectorItem.ts
@@ -17,9 +17,21 @@ const defaultProps: BaseSelectorItemProps = {
 Component({
   props: defaultProps,
 
+  data: {
+    // 折叠控制
+    foldControl: {},
+  },
+
   methods: {
     checkInstance(ref) {
       this.$checkInstance = ref;
+    },
+
+    // 展开和折叠子选项
+    onFoldItemHandler({ target: { dataset } }) {
+      const key = dataset.key;
+      const currentState = this.data.foldControl[key];
+      this.setData({ foldControl: { ...this.data.foldControl, [key]: !currentState } });
     },
 
     // 通过点击body选中check
