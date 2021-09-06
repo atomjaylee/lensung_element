ebbbfe8e03cfadaac6058c1adb8a79564a55bedc
atomjaylee <908105576@qq.com>
2021-09-06 10:27:31 +0800

    feat: fmtEvent方法增加转换驼峰命令

--- packages/_utils/fmtEvent.ts
+++ packages/_utils/fmtEvent.ts
@@ -1,12 +1,26 @@
 export default function fmtEvent(props, e) {
   const dataset = {};
   for (const key in props) {
-    if (/data-/gi.test(key)) {
-      dataset[key.replace(/data-/gi, "")] = props[key];
+    if (/^data-.*/.test(key)) {
+      let dataKey = getKey(key);
+      dataset[dataKey] = props[key];
     }
   }
   return Object.assign({}, e, {
     currentTarget: { dataset },
     target: { dataset, targetDataset: dataset },
   }) as tinyapp.IBaseEvent;
+}
+
+/**
+ * 提取 data- 后的内容，删除 - 且将 - 后的第一个字符改为大写
+ * @param {string} key dataset key eg.data-userId
+ */
+function getKey(key: string): string {
+  return key.replace(/(^data-)|(-)(.)/g, (match, f, s, t) => {
+    if (s && t) {
+      return t.toUpperCase();
+    }
+    return '';
+  });
 }
