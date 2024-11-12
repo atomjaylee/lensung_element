b3e4f45bfc1f69212e08e7d260c86b8dcbff268d
atomjaylee <908105576@qq.com>
2024-11-12 14:35:09 +0800

    fix: 修复dialog底部按钮横线可能不显示的问题

--- examples/pages/dialog/dialog.js
+++ examples/pages/dialog/dialog.js
@@ -1,51 +1,63 @@
 Page({
   data: {
-    hiddenButton: false
+    hiddenButton: false,
   },
-  onLoad() { },
+  onLoad() {},
 
   dialog(ref) {
-    this.$dialog = ref
+    this.$dialog = ref;
   },
 
   async onOpenDialogHandler() {
-    this.$dialog.confirm({
-      content: "显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容",
-      title: "测试标题",
-      maskClosable: true,
-      confirmText: "去订购",
-      onCancel: () => {
-        console.log("11123123")
-      },
-      onBeforeClose: () => {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve(true)
-          }, 0)
-        })
-      }
-    }).then(() => {
-      console.log("confirm")
-    })
+    this.$dialog
+      .confirm({
+        content:
+          '显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容显示内容',
+        title: '测试标题',
+        maskClosable: true,
+        confirmText: '去订购',
+        onCancel: () => {
+          console.log('11123123');
+        },
+        onBeforeClose: () => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(true);
+            }, 0);
+          });
+        },
+      })
+      .then(() => {
+        console.log('confirm');
+      });
   },
 
   sleep(ms) {
-    return new Promise(reslove => setTimeout(reslove, ms))
+    return new Promise((reslove) => setTimeout(reslove, ms));
   },
 
   // 二次调用
   onDialogSecondOpenHandler() {
     this.$dialog.confirm({
-      title: "title",
-      content: "是否二次调用",
+      title: 'title',
+      content: '是否二次调用',
       maskClosable: true,
       hiddenButtons: true,
       onConfirm: () => {
-        this.$dialog.alert({ content: "您点击了确定" })
-      }
-    })
+        this.$dialog.alert({ content: '您点击了确定' });
+      },
+    });
     // this.$dialog.confirm({ content: "是否二次调用" }).then(() => {
     //   this.$dialog.alert({ content: "点击了确定" })
     // })
-  }
+  },
+
+  onDialogAlertHandler() {
+    this.$dialog.alert({
+      isNew: true,
+      title: '温馨提示',
+      content: '检测到您未升级成功，是否付款遇到问题？',
+      confirmText: '咨询客服',
+    });
+  },
 });
